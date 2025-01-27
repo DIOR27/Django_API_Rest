d49d1c2f16164c2392521ffe144a8c2a6ce0430d
Diego Orellana Ávila <diego.orellanaa@ucuenca.edu.ec>
2025-01-27 15:33:50 -0500

    UP: SPOTIFY_REDIRECT_URI

--- api_rest/views.py
+++ api_rest/views.py
@@ -15,7 +15,9 @@ load_dotenv()
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+SPOTIFY_REDIRECT_URI = os.getenv(
+    "SPOTIFY_REDIRECT_URI", "http://127.0.0.1:8000/api/callback/"
+)
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_API_URL = "https://api.spotify.com/v1/me/top/artists"
